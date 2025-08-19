29d7dc1e7980ce95222be9a2398488d844a5afb4
shahariyar23 <shahariyar2307@gmail.com>
2025-08-19 23:37:55 +0600

    replace the cors link instend of  live site  link

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const PORT = process.env.REACT_APP_PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://gym-frontend-zeta.vercel.app",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
