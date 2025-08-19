9f6fa71fb06d4adecbb526fb239c600f19a78318
shahariyar23 <shahariyar2307@gmail.com>
2025-08-19 22:44:03 +0600

    repo change

---
+++ model/Review.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const ReviewSchema = new mongoose.Schema(
+  {
+    productId: String,
+    userId: String,
+    userName: String,
+    reviewMessage: {
+      type: String,
+      set: (v) =>  (v==null ? "No Review": v)    
+    },
+    reviewValue: Number,
+  },
+  { timestamps: true }
+);
+
+const Review = mongoose.model("Review", ReviewSchema);
+
+module.exports = Review;
---
+++ model/course.model.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema(
+  {
+    image: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    gymType: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    offerPrice: {
+      type: Number,
+      default: 0,
+      set: (v) => (v == null ? 0 : v),
+    },
+    averageReview: {
+      type: Number,
+      default: 0,
+      set: (v) => (v==null ? 0: v)
+    },
+  },
+  { timestamps: true }
+);
+
+const Course = mongoose.model("Course", courseSchema);
+
+module.exports = Course;
